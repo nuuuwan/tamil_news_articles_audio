471441be51305f41266ccd82f2d40e6a1ecc29c2
nuuuwan <nuuuwan@gmail.com>
2023-02-26 10:55:42 +0530

    added logging

--- workflows/build_text_and_audio.py
+++ workflows/build_text_and_audio.py
@@ -1,11 +1,18 @@
-from tnaa import TNALibrary, TNAArticle
+from utils import Log
+
+from tnaa import TNAArticle, TNALibrary
+
+log = Log('build_text_and_audio')
+
 
 def main():
     summary_list = TNALibrary().summary_tamil_articles
-    for summary in summary_list:
+    n = len(summary_list)
+    for i, summary in enumerate(summary_list):
         article = TNAArticle.from_hash(summary['hash'])
         article.save_text()
         article.save_audio()
+        log.info(f'{i+1}/{n} {article.title} complete.')
 
 
 if __name__ == '__main__':
