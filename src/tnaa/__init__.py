7aa3524603c0629e5f5e95c13d62a58911abafe0
nuuuwan <nuuuwan@gmail.com>
2023-02-26 20:45:46 +0530

    fix: render import BUG

--- src/tnaa/__init__.py
+++ src/tnaa/__init__.py
@@ -1,2 +1,5 @@
 from tnaa.core.TNAArticle import TNAArticle
 from tnaa.core.TNALibrary import TNALibrary
+
+from tnaa.render.ArticlePage import ArticlePage
+from tnaa.render.IndexPage import IndexPage
--- src/tnaa/render/__init__.py
+++ src/tnaa/render/__init__.py
@@ -1,2 +1,1 @@
-from tnaa.render.ArticlePage import ArticlePage
-from tnaa.render.IndexPage import IndexPage
+
